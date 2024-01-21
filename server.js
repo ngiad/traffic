4656acf6b478131b2e15f073bea6ac1f793eadd2
ngiad <follownghia001@gmail.com>
2024-01-21 23:03:55 +0700

    setup app server

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 const { default: mongoose } = require("mongoose");
-const start = require("./app");
+const start = require("./api/app");
 const PORT = process.env.PORT || 5656;
 
 const server = start(PORT)
 
-process.on("SIGINT",async() => {
+process.on("SIGINT", async () => {
     console.log("\nReceived SIGINT. Closing server and disconnecting from MongoDB...");
     try {
-      await server.close();
-      console.log("Express server closed.");
-      
-      await mongoose.disconnect();
-      console.log("MongoDB connection closed.");
-      
-      process.exit(0);
+        await server.close();
+        console.log("Express server closed.");
+
+        await mongoose.disconnect();
+        console.log("MongoDB connection closed.");
+
+        process.exit(0);
     } catch (err) {
-      console.error("Error during shutdown:", err);
-      process.exit(1);
+        console.error("Error during shutdown:", err);
+        process.exit(1);
     }
-  })
+})
