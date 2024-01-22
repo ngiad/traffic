e2da67190b9ddf3e9e14dac557da8dbfe33fd886
ngiad <follownghia001@gmail.com>
2024-01-22 10:53:33 +0700

    setup file layout for admin dashboard marketing

---
+++ src/app/(Marketing)/layout.js
@@ -0,0 +1,14 @@
+
+export const metadata = {
+    title: "Trang chủ",
+    description: "Smart traffic",
+};
+
+
+const layout = ({ children }) => {
+    return (
+        <div>{children}</div>
+    )
+}
+
+export default layout
