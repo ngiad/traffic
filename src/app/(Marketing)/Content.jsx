6941ad987bc0fb666b413839676191cb4f55e5e8
NguyenTatDuy <tatduy27112002@gmail.com>
2024-01-30 02:40:23 +0700

    update1

---
+++ src/app/(Marketing)/Content.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import styles from "../../assets/styles/menber.module.css";
+function Content() {
+  return (
+    <div>
+      <div className={styles.page1}>
+        <div className={styles.left}>
+          <h1 className={styles.h1}>
+            Save time by building h1 fast with Boldo Template{" "}
+          </h1>
+          <p className={styles.p}>
+            Funding handshake buyer business-to-business metrics iPad
+            partnership. First mover advantage innovator success deployment
+            non-disclosure.
+          </p>
+          <div className={styles.button}>
+            <button className={styles.button56} role="button">
+              Buy template
+            </button>
+            <button className={styles.button56} role="button">
+              Explore
+            </button>
+          </div>
+        </div>
+        <div className={styles.right}>b</div>
+      </div>
+      <div>a</div>
+    </div>
+  );
+}
+
+export default Content;
