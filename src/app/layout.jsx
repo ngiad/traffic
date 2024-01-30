6c476a3e391eee258add7304b82eaaa6dc4619da
HSonDev74 <lesonkyac@gmail.com>
2024-01-30 18:40:43 +0700

    main: edit layout body not use css inter

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -6,18 +6,18 @@ import Cookies from "@/components/Providers/Cookies";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Sorni link",
-  description: "Smart traffic",
+     title: "Sorni link",
+     description: "Smart traffic",
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <Cookies>
-      <ReduxProvider>
-        <html lang="en">
-          <body className={inter.className}>{children}</body>
-        </html>
-      </ReduxProvider>
-    </Cookies>
-  );
+     return (
+          <Cookies>
+               <ReduxProvider>
+                    <html lang="en">
+                         <body>{children}</body>
+                    </html>
+               </ReduxProvider>
+          </Cookies>
+     );
 }
